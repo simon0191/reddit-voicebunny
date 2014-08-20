3f6bd58f20a2c6741475bfb6ed4c657ca80fffac
simon0191 <simon0191@gmail.com>
2014-08-20 12:05:53 -0500

    Randomize reddit article

--- app/adapters/articlesAdapter.js
+++ app/adapters/articlesAdapter.js
@@ -1,10 +1,27 @@
 var requestify = require('requestify');
+var _ = require('underscore');
 
 var articlesAdapter = (function() {
-  return {
-    getTrendy: function(callback) {
 
-      requestify.get('http://www.reddit.com/r/funny/hot.json?limit=1').then(function(response) {
+  var getSubreddits = function(callback) {
+    requestify.get('http://www.reddit.com/subreddits/popular.json?limit=20').then(function(response) {
+        // Get the response body
+        var listing = response.getBody().data;
+        var subreddits = listing.children;
+        var result = _.map(subreddits,function(sr){
+          return {
+            title: sr.data.title,
+            url: sr.data.url
+          };
+        });
+        callback(null,result);
+    });
+  };
+
+  var getTrendy = function(callback) {
+    getSubreddits(function(err,subreddits) {
+      var sr = subreddits[_.random(0,subreddits.length-1)];
+      requestify.get('http://www.reddit.com'+sr.url+'hot.json?limit=1').then(function(response) {
           // Get the response body
           var title = response.getBody().data.children[0].data.title;
           var url = response.getBody().data.children[0].data.url;
@@ -14,7 +31,13 @@ var articlesAdapter = (function() {
           });
 
       });
-    }
+    });
+  };
+
+  return {
+    getTrendy: getTrendy,
+    getSubreddits: getSubreddits
+
   };
 
 })();
