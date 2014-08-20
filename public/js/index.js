19d8d0d5bfdae4c82e188bc69128e063e00697fa
simon0191 <simon0191@gmail.com>
2014-08-20 00:42:42 -0500

    Creation of speedy projects in test mode

--- public/js/index.js
+++ public/js/index.js
@@ -18,7 +18,51 @@ $(document).ready(function(){
   //Create Speedy
   $('#create-vb-speedy').on('click',function(event) {
     event.preventDefault();
+    if(validateForm()) {
+      var postData = getPostData();
 
+
+      $.post('/projects', postData, function(data) {
+        var project = data.project;
+        var links = _.map(project.reads[0].urls,function(url,name) {
+          return $('<a/>',{
+            href:url.default,
+            text: name
+          });
+        });
+        $('#links-container').html(links);
+        $('#notification-modal').modal('show');
+
+        console.log(data);
+      }, "json");
+    }
+  });
+
+
+
+
+  // Helper methods
+  function validateForm() {
+    var requiredFormFields = ['title','script'];
+    var invalid = false;
+    _.each(requiredFormFields,function(field) {
+      var elem = $('#'+field);
+      if(elem.length > 0) {
+        var val = elem.val();
+        if(!val) {
+          invalid = true;
+        }
+      }
+    });
+    if(invalid) {
+      $('#error-modal').modal('show');
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function getPostData() {
     var formFields = ['title','script','language','gender-and-age','lifetime','remarks','ping','price'];
     var postData = {};
     _.each(formFields,function(field) {
@@ -31,9 +75,7 @@ $(document).ready(function(){
       }
     });
 
-    $.post('/projects', postData, function(data) {
-      console.log(data);
-    }, "json");
-  });
+    return postData;
+  }
 
 });
